3ac945f72f8e9ed2a4a208ff74710cb53d4b7e2b
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-08 10:22:12 -0300

    chore: set up the test environment and database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
-require('dotenv/config');
+require('../bootstrap');
 
 module.exports = {
-  dialect: 'mysql',
+  dialect: process.env.DB_DIALECT || 'mysql',
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
+  storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
     underscored: true,
