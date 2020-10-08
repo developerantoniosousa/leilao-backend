9b4ac5c5ec7b6b61bfc48940b989866ef3f22975
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-08 19:54:37 -0300

    feat: add create_auctions migration

---
+++ src/database/migrations/20201008223441-create-auction.js
@@ -0,0 +1,48 @@
+const tableName = 'auction';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable(tableName, {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.FLOAT,
+      },
+      is_used: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      responsabler_id: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'users' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      completed_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable(tableName);
+  },
+};
