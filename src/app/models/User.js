a18ab86ac3779fab07ddbc51e87374cafc034354
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-08 16:41:42 -0300

    feat: password encryption

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,15 +1,24 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         active: Sequelize.BOOLEAN,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
   }
 }
